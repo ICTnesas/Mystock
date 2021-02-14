4362105599aeda411872aad6ec808c12cd05a595
adrian <muhamadadriangofur@gmail.com>
2021-02-14 14:40:57 +0700

    bikin log in page

---
+++ application/views/auth/login.php
@@ -0,0 +1,44 @@
+    <div class="container">
+
+        <!-- Outer Row -->
+        <div class="row justify-content-center">
+
+            <div class="col-7">
+
+                <div class="glass o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        <div class="row">
+                            <div class="col-lg">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1 class="h1 title-login">Mystock</h1>
+                                    </div>
+                                    <div class="text-center">
+                                        <h1 class="h1 login">Log In</h1>
+                                    </div>
+                                    <form class="user">
+                                        <div class="form-group">
+                                            <input type="email" class="form-control form-control-user" id="nisn" name="nisn" aria-describedby="emailHelp" placeholder="NISN">
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
+                                        </div>
+                                        <hr class="">
+                                        <submit href="index.html" class="btn btn-login btn-block">
+                                            Login
+                                        </submit>
+                                        <submit href="index.html" class="btn btn-login btn-block">
+                                            Sign Up
+                                        </submit>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
---
+++ application/views/templates/header.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/97dfded217.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="<?= base_url('assets/') ?>css/style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800&display=swap" rel="stylesheet">
+
+    <title><?= $title ?></title>
+</head>
+
+<body>
+    
